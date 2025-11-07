7d9eea626c431c1636e1a00b9f9febba0b27cc03
Tkarsikas <tommi.karsi@gmail.com>
2025-11-07 15:52:36 +0200

    Kellon testausta

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template
 import mysql.connector
+
 app = Flask(__name__)
-@app.route('/')
+
 def home():
  # Connect to MySQL/MariaDB
  conn = mysql.connector.connect(
@@ -10,13 +11,29 @@ def home():
  password="moccamaster",
  database="kurssi_LEMP"
  )
- cursor = conn.cursor()
- cursor.execute("SELECT 'testisivu toimii!'")
- cursor.execute("SELECT CONCAT('testisivu toimii! ', DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s'))")
+ 
+@app.route('/')
+ def api_time():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute("SELECT NOW();")
+    row = cur.fetchone()
+    cur.close()
+    conn.close()
+    if row:
+        # row[0] on datetime-objekti tai string, jsonify hoitaa mm. str-muunnoksen
+        return jsonify({"time": str(row[0])})
+    return jsonify({"time": None}), 500
+
+if __name__ == '__main__':
+ app.run(host='0.0.0.0', port=5000)
+
+
+ /* cursor = conn.cursor()
+ cursor.execute("SELECT NOW()")
  result = cursor.fetchone()
  # Clean up
  cursor.close()
  conn.close()
  return f"<h1>{result[0]}</h1>"
-if __name__ == '__main__':
- app.run(host='0.0.0.0', port=5000)
+*/
