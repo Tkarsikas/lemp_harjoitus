1a06bc7c7e311b9287d69169840be3302a7e29a5
Tkarsikas <tommi.karsi@gmail.com>
2025-11-07 16:29:47 +0200

    kello

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def home():
  database="kurssi_LEMP"
  )
  cursor = conn.cursor()
- cursor.execute("SELECT 'Hello from MySQL!'")
+ cursor.execute("SELECT NOW();")'")
  result = cursor.fetchone()
  # Clean up
  cursor.close()
