084fc62367857cf357daf70cef4575b36aa233f5
Tommi <tommi.karsi@gmail.com>
2025-11-13 04:57:35 +0200

    lisätty penkki taulukko

--- streamlitapp.py
+++ streamlitapp.py
@@ -6,17 +6,23 @@ def mySql():
     # Initialize connection.
     conn = st.connection('mysql', type='sql')
     # Perform query.
-    df = conn.query('SELECT viikko, nimi, toistot, paino from kyykky LIMIT 100;', ttl=600)
-    return df
+    df_kyykky = conn.query('SELECT viikko, nimi, toistot, paino from kyykky LIMIT 100;', ttl=600)
+    df_penkki = conn.query('SELECT viikko, nimi, toistot, paino from penkki LIMIT 100;', ttl=600)
+    df_kyykky['harjoituslaji'] = 'kyykky'
+    df_penkki['harjoituslaji'] = 'penkki'
+    
+    return df_kyykky, df_penkki
     # Streamlit
 def main():
     st.title("Plot data from MySql")
     st.write("kyykky kehitys viikkojen mukaan")
-    data = mySql()
+    df_kyykky, df_penkki = mySql()
     #plot data
-    if not data.empty:
-        fig = px.line(data, x="viikko", y="paino", color="nimi",
-                      title='Kyykky kehitys viikkojen mukaan', markers=True)
+    if not df_kyykky.empty or not df_penkki.empty:
+        df_combined = pd.concat([df_kyykky, df_penkki], ignore_index=True)
+        fig = px.line(df_combined, x="viikko", y="paino", color="nimi",
+                      line_dash="harjoituslaji",
+                      title='harjoitus painot viikottain', markers=True)
         st.plotly_chart(fig, use_container_width=True)
     else:
         st.write("Ei dataa näytettäväksi")
