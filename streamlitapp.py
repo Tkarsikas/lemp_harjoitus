c60648e1a93574bb47b9d78a4a154ca39d24621c
Tommi <tommi.karsi@gmail.com>
2025-11-13 03:52:13 +0200

    muutoksia

--- streamlitapp.py
+++ streamlitapp.py
@@ -6,16 +6,16 @@ def mySql():
     # Initialize connection.
     conn = st.connection('mysql', type='sql')
     # Perform query.
-    df = conn.query('SELECT viikko, nimi, toistot from treenit LIMIT 100;', ttl=600)
+    df = conn.query('SELECT viikko, nimi, toistot from kyykky LIMIT 100;', ttl=600)
     return df
     # Streamlit
 def main():
     st.title("Plot data from MySql")
-    st.write("progression of weight lifting over weeks")
+    st.write("kyykky kehitys viikkojen mukaan")
     data = mySql()
     #plot data
     df2 = pd.DataFrame(data, columns=["viikko", "nimi", "toistot"])
-    totalKg = px.line(df2, x=df2.index, y="toistot", title='Total weight lifted over weeks')
+    totalKg = px.line(df2, x=df2.index, y="toistot", title='kyykky kehitys')
     st.plotly_chart(totalKg, use_container_width=True)
 if __name__ == "__main__":
     main()
